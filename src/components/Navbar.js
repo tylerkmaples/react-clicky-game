01adda4374ed6c054710a6bb2881499028453cd1
TKMCode <tyler.code.maples@gmail.com>
2019-03-30 20:42:34 -0700

    initial set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar(props) {
+    return (
+        <nav class="navbar navbar-expanded-lg navbar-dark bg-dark">
+
+        </nav>
+    )
+}
+
+export default Navbar;
